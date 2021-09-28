446a204ea84e3079f5718e71c030f89876c5d589
Danial Bleile <danbleile@gmail.com>
2021-09-28 10:40:27 -0700

    Enhancement | Add hero banner banner

--- src/bundles/wsu-design-system.js
+++ src/bundles/wsu-design-system.js
@@ -2,11 +2,11 @@
 import WsuMenuToggle from "../components/menu-toggle/_menu-toggle";
 import WsunavigationSite from '../modules/navigation-site/_navigation-site';
 import WsuHeaderGlobal from "../modules/header-global/_header-global";
-import WsuAccordion from "../components/accordion/script";
-import WsuCollapsable from "../components/collapsable/script";
+import WsuAccordion from "../components/_experimental_accordion/script";
+import WsuCollapsable from "../components/_experimental_collapsable/script";
 import WsuSubmenuToggle from "../components/submenu-toggle/_script";
 import WsuMenu from "../components/menu/_script";
-import WsuNavigationSiteVertical from "../modules/experimental__navigation-site-horizontal/_script";
+import WsuNavigationSiteVertical from "../modules/_experimental_navigation-site-vertical/_script";
 
 
 
