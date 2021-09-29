df942016a5110410688643132ee93577db81da08
Danial Bleile <danbleile@gmail.com>
2021-09-29 10:48:06 -0700

    Cleanup | Rebuild dist folder

--- src/bundles/wsu-design-system.js
+++ src/bundles/wsu-design-system.js
@@ -1,6 +1,6 @@
 //import wsuDropdownModal from "../elements/dropdown-modal/_dropdown-modal";
 import WsuMenuToggle from "../components/menu-toggle/_menu-toggle";
-import WsunavigationSite from '../modules/navigation-site/_navigation-site';
+import WsunavigationSite from '../modules/deprecated_navigation-site/_navigation-site';
 import WsuHeaderGlobal from "../modules/header-global/_header-global";
 import WsuAccordion from "../components/experimental_accordion/script";
 import WsuCollapsable from "../components/experimental_collapsable/script";
