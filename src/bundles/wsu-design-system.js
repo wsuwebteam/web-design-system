4eee237cb0dfa5bc6ac2d0c3977a022d8c636c5e
Danial Bleile <danbleile@gmail.com>
2021-10-01 16:11:38 -0700

    Restructure | Clean up components

--- src/bundles/wsu-design-system.js
+++ src/bundles/wsu-design-system.js
@@ -1,12 +1,10 @@
 //import wsuDropdownModal from "../elements/dropdown-modal/_dropdown-modal";
-import WsuMenuToggle from "../components/menu-toggle/_menu-toggle";
 import WsunavigationSite from '../modules/deprecated_navigation-site/_navigation-site';
 import WsuHeaderGlobal from "../modules/header-global/_header-global";
 import WsuAccordion from "../components/experimental_accordion/script";
 import WsuCollapsable from "../components/experimental_collapsable/script";
-import WsuSubmenuToggle from "../components/submenu-toggle/_script";
 import WsuMenu from "../components/menu/_script";
-import WsuNavigationSiteVertical from "../modules/experimental_navigation-site-vertical/_script";
+import WsuNavigationSiteVertical from "../modules/navigation-site-vertical/_script";
 
 
 
@@ -15,10 +13,8 @@ import WsuNavigationSiteVertical from "../modules/experimental_navigation-site-v
 const wsu = {
     verticalNavitation: new WsunavigationSite(),
     navigationSiteVertical: new WsuNavigationSiteVertical(),
-    menuToggle: new WsuMenuToggle(),
     headerGlobal: new WsuHeaderGlobal(),
     accordion: new WsuAccordion(),
     collapsable: new WsuCollapsable(),
-    submenuToggle: new WsuSubmenuToggle(),
     menu: new WsuMenu(),
 } 
