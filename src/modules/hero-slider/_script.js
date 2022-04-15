4558439394b2ce3b4f1fe380cea1d055858d0d9a
Leilani Gomoto <leilani.gomoto@freshconsulting.com>
2022-04-15 09:56:20 -0700

    Add autoplay, pause button; update spans to buttons

--- src/modules/hero-slider/_script.js
+++ src/modules/hero-slider/_script.js
@@ -1,16 +1,21 @@
-import Swiper, { Navigation, Pagination, EffectFade } from 'swiper';
+import Swiper, { Navigation, Pagination, EffectFade, Autoplay } from 'swiper';
 
-Swiper.use( [ Navigation, Pagination, EffectFade ] );
+Swiper.use( [ Navigation, Pagination, EffectFade, Autoplay ] );
 
 const initSwiper = () => {
   let slider = document.getElementsByClassName('swiper');
 
   if ( slider.length > 0 ) {
     let slideTitles = document.querySelectorAll('.swiper-slide .wsu-title');
+    let pauseButton = document.getElementById('slider-pause');
 
     const swiper = new Swiper('.swiper', {
       effect: 'fade',
       slidesPerView: 1,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
       navigation: {
         nextEl: ".wsu-i-arrow-right-carrot",
         prevEl: ".wsu-i-arrow-left-carrot",
@@ -19,8 +24,20 @@ const initSwiper = () => {
         el: ".wsu-hero-slider__pagination",
         clickable: true,
         renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + slideTitles[index].innerText + "</span>";
+          return '<button class="' + className + '">' + slideTitles[index].innerText + "</button>";
         },
+      },
+    });
+
+    pauseButton.addEventListener('click', function() {
+      if ( pauseButton.classList.contains('play') ) {
+        pauseButton.classList.remove('play');
+        pauseButton.classList.add('paused');
+        swiper.autoplay.stop();
+      } else {
+        pauseButton.classList.remove('paused');
+        pauseButton.classList.add('play');
+        swiper.autoplay.start();
       }
     });
   }
