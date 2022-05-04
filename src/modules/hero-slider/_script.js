499c82699120c3c0401f7bd4202257e8c1426a32
Dan White <dan.white1@wsu.edu>
2022-05-04 13:29:07 -0700

    Implement refactoring for Gutenberg Integration

--- src/modules/hero-slider/_script.js
+++ src/modules/hero-slider/_script.js
@@ -1,80 +1,72 @@
-import Swiper, { Navigation, Pagination, Autoplay, EffectFade } from 'swiper';
+import Swiper, { Navigation, Pagination, Autoplay, EffectFade } from "swiper";
 
-Swiper.use( [ Navigation, Pagination, Autoplay, EffectFade ] );
+Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
 
 const initSwiper = () => {
-  const sliderName = '.swiper';
-  let heroSliders = document.querySelectorAll( sliderName );
+  const sliderName = ".swiper";
+  let heroSliders = document.querySelectorAll(sliderName);
 
-  if ( heroSliders.length > 0 ) {
-    heroSliders.forEach( ( slider, index ) => {
-      let heroSlider = slider.querySelector( '.swiper-wrapper' );
-      let slides = heroSlider.querySelectorAll( '.wsu-hero' );
+  if (heroSliders.length > 0) {
+    heroSliders.forEach((slider, index) => {
+      const autoplay = slider.dataset.autoplay === "true" ? true : false;
 
-      let slideTitles = document.querySelectorAll( '.swiper-slide .wsu-title' );
-      let pauseButton = document.getElementById( 'slider-pause' );
+      let heroSlider = slider.querySelector(".swiper-wrapper");
+      let slides = heroSlider.querySelectorAll(".wsu-hero");
 
-      if ( slides.length > 1 ) {
-        let autoplayDelay = '';
-        slider.classList.add( `swiper-${index}` );
+      let slideTitles = slider.querySelectorAll(".swiper-slide .wsu-title");
+      let pauseButton = slider.querySelector(".wsu-hero-slider__pause-btn");
 
-        if ( slides[`${index}`].classList.contains( 'swiper-autoplay' ) ) {
-          autoplayDelay = slides[0].getAttribute( 'data-swiper-autoplay' );
+      if (slides.length > 1) {
+        slider.classList.add(`swiper-${index}`);
 
-          const swiper = new Swiper( `.swiper-${index}` , {
-            effect: 'fade',
-            slidesPerView: 1,
-            autoplay: {
-              delay: autoplayDelay,
-              disableOnInteraction: false,
+        const swiperSettings = {
+          effect: "fade",
+          slidesPerView: 1,
+          navigation: {
+            nextEl: ".wsu-i-arrow-right-carrot",
+            prevEl: ".wsu-i-arrow-left-carrot",
+          },
+          pagination: {
+            el: ".wsu-hero-slider__pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+              return (
+                '<button class="' +
+                className +
+                '">' +
+                slideTitles[index].textContent +
+                "</button>"
+              );
             },
-            navigation: {
-              nextEl: ".wsu-i-arrow-right-carrot",
-              prevEl: ".wsu-i-arrow-left-carrot",
-            },
-            pagination: {
-              el: ".wsu-hero-slider__pagination",
-              clickable: true,
-              renderBullet: function (index, className) {
-                return '<button class="' + className + '">' + slideTitles[index].textContent + "</button>";
-              },
-            },
-          });
+          },
+        };
 
-          pauseButton.addEventListener('click', function() {
-            if ( pauseButton.classList.contains('play') ) {
-              pauseButton.classList.remove('play');
-              pauseButton.classList.add('paused');
+        if (autoplay) {
+          swiperSettings.autoplay = {
+            delay: slider.dataset.autoplayDelay || "5000",
+            disableOnInteraction: false,
+            pauseOnMouseEnter: false,
+          };
+        }
+
+        const swiper = new Swiper(`.swiper-${index}`, swiperSettings);
+
+        if (autoplay) {
+          pauseButton.addEventListener("click", function () {
+            if (pauseButton.classList.contains("play")) {
+              pauseButton.classList.remove("play");
+              pauseButton.classList.add("paused");
               swiper.autoplay.stop();
             } else {
-              pauseButton.classList.remove('paused');
-              pauseButton.classList.add('play');
+              pauseButton.classList.remove("paused");
+              pauseButton.classList.add("play");
               swiper.autoplay.start();
             }
-          });
-
-        } else {
-          const swiper = new Swiper( `.swiper-${index}` , {
-            effect: 'fade',
-            slidesPerView: 1,
-            navigation: {
-              nextEl: ".wsu-i-arrow-right-carrot",
-              prevEl: ".wsu-i-arrow-left-carrot",
-            },
-            pagination: {
-              el: ".wsu-hero-slider__pagination",
-              clickable: true,
-              renderBullet: function (index, className) {
-                return '<button class="' + className + '">' + slideTitles[index].textContent + "</button>";
-              },
-            },
           });
         }
       }
     });
   }
-}
+};
 
-export default () => {
-	initSwiper();
-}
+initSwiper();
