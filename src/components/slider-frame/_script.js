7e001e00f54198ad65821bf89e1ebe631b3f4864
Dan White <dan.white1@wsu.edu>
2022-06-14 16:51:19 -0700

    Enhancement | Implement  pausing/resuming

--- src/components/slider-frame/_script.js
+++ src/components/slider-frame/_script.js
@@ -26,7 +26,24 @@ const initSliderFrame = () => {
       sliderFrame.classList.add(`wsu-slider-frame-${index}`);
       const instance = new Swiper(`.wsu-slider-frame-${index}`, swiperSettings);
     });
+
+    handlePauseButtonClicks();
   }
+};
+
+const handlePauseButtonClicks = () => {
+  document.addEventListener("click", function (event) {
+    const target = event.target;
+    const classList = target.classList;
+
+    if (classList.contains("wsu-slider-frame__pause-button")) {
+      if (classList.contains("wsu-button-pause-background--active")) {
+        target.parentElement.swiper?.autoplay.start();
+      } else {
+        target.parentElement.swiper?.autoplay.stop();
+      }
+    }
+  });
 };
 
 initSliderFrame();
