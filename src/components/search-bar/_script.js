8b5817eea788a6edf96efdf578c9348bfd6e9595
Leilani Gomoto <leilani.gomoto@freshconsulting.com>
2022-06-22 15:24:57 -0700

    WIP | Search Box and Results

--- src/components/search-bar/_script.js
+++ src/components/search-bar/_script.js
@@ -9,6 +9,7 @@ class WsuSearchBar {
     init() {
 
         this.bindEvents();
+        this.searchParam();
 
     }
 
@@ -36,13 +37,50 @@ class WsuSearchBar {
 
 					eventElement.closest('.wsu-search-bar__scope-option').setAttribute('aria-checked', 'true');
 				});
-			}
+
+        var div = eventElement.closest('.wsu-search-bar__scope-option');
+
+        this.displayElements( div );
+      }
 
 		} catch (error) {
 		  console.error(error);
 		}
 
 	}
+
+  searchParam() {
+    var searchbox = document.getElementsByClassName('gcse-searbox')[0];
+    console.log(searchbox);
+
+    var site = window.location.hostname;
+    console.log(site);
+
+  }
+
+  displayElements( div ) {
+    console.log(div);
+    var allBox = document.getElementsByClassName('wsu-search-bar--all')[0];
+    var singleBox = document.getElementsByClassName('wsu-search-bar--single')[0];
+
+    var allResults = document.getElementsByClassName('wsu-search-results--all')[0];
+    var singleResults = document.getElementsByClassName('wsu-search-results--single')[0];
+
+    if ( div.getAttribute('id') === 'all' ) {
+      allBox.setAttribute('aria-hidden', 'false');
+      allResults.setAttribute('aria-hidden', 'false');
+
+      singleBox.setAttribute('aria-hidden', 'true');
+      singleResults.setAttribute('aria-hidden', 'true');
+
+    } else {
+      allBox.setAttribute('aria-hidden', 'true');
+      allResults.setAttribute('aria-hidden', 'true');
+
+      singleBox.setAttribute('aria-hidden', 'false');
+      singleResults.setAttribute('aria-hidden', 'false');
+    }
+  }
 
 }
 
