2927cc817afd5109b4aa0febbe58c2c24b703e87
Matthew Davis <matthew.a.davis@wsu.edu>
2022-07-19 16:01:47 -0700

    Enhancements | Modal

--- src/components/modal/_script.js
+++ src/components/modal/_script.js
@@ -13,16 +13,30 @@ class WsuModal {
 
     bindEvents() {
         let modal = document.querySelector(".wsu-modal")
+        let openButton = document.getElementById("modal-open");
+        let closeButton = document.querySelector(".wsu-modal__close-button");
 
-		document.getElementById("modal-open").onclick = function(){
-            modal.style.display = "block"
+		openButton.onclick = function(){
+            modal.classList.toggle("wsu-modal__closed");
+            //document.body.setAttribute("overflow", "hidden");
+            document.body.classList.toggle("modal-open");
+            document.getElementById("modalTitle").focus();
+
+            //TODO: set focus to modal
+            //set screen reader focus
+            //prevent scroll outside modal
         }
-        document.querySelector(".wsu-modal__close-button").onclick = function(){
-            modal.style.display = "none"
+        closeButton.onclick = function(){
+            modal.classList.toggle("wsu-modal__closed");
+            //document.body.setAttribute("overflow", "scroll");
+            document.body.classList.toggle("modal-open");
+            //release focus/scroll locks
         }
         window.onclick = function(e){
             if(e.target == modal){
-              modal.style.display = "none"
+                modal.classList.toggle("wsu-modal__closed");
+                //document.body.setAttribute("overflow", "scroll");
+                document.body.classList.toggle("modal-open");
             }
         }
 	}
