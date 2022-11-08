73c346331a7998ad6bca650aded1d0ca9830da9d
Danial Bleile <danbleile@gmail.com>
2022-11-08 05:28:40 -0800

    Add Module | Navigation Vertical

---
+++ src/modules/navigation-vertical/_script.js
@@ -0,0 +1,117 @@
+import updateAriaElement from "../../../_assets/js/updateAriaElement";
+import { elementGetSiblings } from "../../../_assets/js/partials/element";
+
+class WsuNavigationVertical {
+
+    
+        constructor( atts = {} ) {
+
+            this.headers = ['.wsu-header-unit'];
+
+            this.navigation = false;
+
+            this.siteHeader = false;
+    
+            this.init();
+            
+        }
+    
+        init() {
+    
+            this.bindEvents();
+    
+        }
+    
+        bindEvents() {
+            document.addEventListener(
+                'scroll', 
+                this.scrollEvent.bind( this ),
+                false
+            );
+    
+            /*document.addEventListener(
+                'keydown', 
+                this.keyDownEvents.bind( this ),
+                false
+            );*/
+        }
+    
+        scrollEvent( event ) {
+            
+            try {
+                
+                this.scrollNav();
+                
+            } catch (error) {
+              console.error(error);
+            }
+            
+        }
+
+        scrollNav() {
+
+            this.setNavigation();
+
+            let height = this.getHeight();
+
+            if ( this.navigation ) {
+
+                this.navigation.style.paddingTop = height + 'px';
+
+            }
+
+        }
+
+        setHeader() {
+
+            if ( ! this.siteHeader ) {
+
+                for ( let h = 0; h < this.headers.length; h++ ) {
+
+                    let header = document.querySelector( this.headers[h] );
+    
+                    if ( header ) {
+    
+                        this.siteHeader = header;
+                        
+                        break;
+    
+                    }
+    
+                }
+
+            }
+
+        }
+
+        setNavigation() {
+
+            if ( ! this.navigation ) {
+
+                this.navigation = document.querySelector( '.wsu-navigation-vertical__panel' );
+
+            }
+
+        }
+    
+        
+        getHeight() {
+
+            this.setHeader();
+
+            let height = 0;
+
+            if ( this.siteHeader ) {
+
+                let headerHeight = this.siteHeader.scrollHeight;
+
+                height = this.siteHeader.getBoundingClientRect().top + headerHeight;
+
+            }
+
+            return height;
+        }
+    
+    }
+    
+    export default WsuNavigationVertical; 
