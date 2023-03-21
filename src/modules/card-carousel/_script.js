7c998aefdf3f2f587163c23ff695158fc72d3c8f
Matthew Davis <matthew.a.davis@wsu.edu>
2023-03-21 13:56:12 -0700

    Enhancement | Card Carousel | Ensured proper TAB navigation

--- src/modules/card-carousel/_script.js
+++ src/modules/card-carousel/_script.js
@@ -1,25 +1,25 @@
 import Swiper, { Navigation, Pagination, Autoplay, EffectFade } from "swiper";
+//Test
 
 Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
-
 const initSwiper = () => {
 
     const cardCarousel = new Swiper(".wsu-card-carousel", {
         slidesPerView: 3,
         spaceBetween: 30,
+        pagination: {
+          el: ".wsu-card-carousel__pagination",
+          clickable: false,
+        },
         navigation: {
           nextEl: '.wsu-card-carousel__button-next',
           prevEl: '.wsu-card-carousel__button-prev',
         },
-        pagination: {
-          el: ".wsu-card-carousel__pagination",
-        },
-        mousewheel: true,
-        keyboard: {
+        a11y: {
           enabled: true,
-          onlyInViewport: true,
+          prevSlideMessage: 'Previous slide',
+          nextSlideMessage: 'Next slide',
         },
-        allowTouchMove: true,
         breakpoints: {
           // when window width is >= 320px
           320: {
@@ -42,8 +42,49 @@ const initSwiper = () => {
             slidesPerView: 3,
             spaceBetween: 40
           },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 40
+          },
         },
     });
-    
+
+
+    // // Gather all focusable elements in a list
+    // let carouselQuery = document.querySelector(".wsu-card-carousel");
+    // const KEYCODE_TAB = 9;
+    // var activeSlide = document.querySelector(".swiper-slide-active .wsu-news-card__content .wsu-article-title a");
+    // var lastSlideInFrame = document.querySelector(".swiper-slide-next + .swiper-slide .wsu-news-card__content .wsu-article-title a");
+    // var prevButton = document.querySelector(".wsu-card-carousel__button-prev");
+    // var nextButton = document.querySelector(".wsu-card-carousel__button-next");
+
+    // carouselQuery.addEventListener("keydown", function (e) {
+    //   var isTabPressed = e.key === "Tab" || e.keyCode === KEYCODE_TAB;
+
+    //   // Define behaviour for Tab or Shift+Tab
+    //   if (isTabPressed) {
+    //     console.log("tab pressed");
+    //       // Shift+Tab
+    //       if (e.shiftKey) {
+    //           if (document.activeElement === activeSlide) {
+
+    //             // NEED TO RESET FOCUS ON THE ELEMENT JUST BEFORE RIGHT ARROW.
+    //               // prevButton.focus();
+    //               // console.log("shift tab pressed");
+    //               // e.preventDefault();
+    //           }
+    //       }
+
+    //       // Tab
+    //       else {
+    //           if (document.activeElement === lastSlideInFrame) {
+    //               nextButton.focus();
+    //               console.log("tabbed on last element");
+    //               e.preventDefault();
+    //           }
+    //       }
+    //   }
+    // });
+
 }
 initSwiper(); 
