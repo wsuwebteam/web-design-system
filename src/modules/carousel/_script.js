029eb783e01c1fef37ddcef59221f18e41c667c9
Matthew Davis <matthew.a.davis@wsu.edu>
2023-03-28 11:27:51 -0700

    Enhancement | Carousel | Updated column attributes

--- src/modules/carousel/_script.js
+++ src/modules/carousel/_script.js
@@ -3,51 +3,58 @@ import Swiper, { Navigation, Pagination, Autoplay, EffectFade } from "swiper";
 
 Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
 const initSwiper = () => {
+  const carouselName = ".wsu-carousel";
+  let carouselSliders = document.querySelectorAll(carouselName);
 
-    const carousel = new Swiper(".wsu-carousel", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        pagination: {
-          el: ".wsu-carousel__pagination",
-          clickable: false,
-        },
-        navigation: {
-          nextEl: '.wsu-carousel__button-next',
-          prevEl: '.wsu-carousel__button-prev',
-        },
-        a11y: {
-          enabled: true,
-          prevSlideMessage: 'Previous slide',
-          nextSlideMessage: 'Next slide',
-        },
-        breakpoints: {
-          // when window width is >= 320px
-          320: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-            navigation: {
-              enabled: false,
+  if (carouselSliders.length > 0) {
+
+    carouselSliders.forEach((carousel, index) => {
+
+      carousel.classList.add(`wsu-carousel-${index}`);
+
+      const swiperSettings = {
+        slidesPerView: carousel.dataset.desktopColumns,
+          spaceBetween: 30,
+          pagination: {
+            el: ".wsu-carousel__pagination",
+            clickable: false,
+          },
+          navigation: {
+            nextEl: '.wsu-carousel__button-next',
+            prevEl: '.wsu-carousel__button-prev',
+          },
+          a11y: {
+            enabled: true,
+            prevSlideMessage: 'Previous slide',
+            nextSlideMessage: 'Next slide',
+          },
+          breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+ 
+            },
+            // when window width is >= 640px
+            768: {
+              slidesPerView: carousel.dataset.tabletColumns,
+              spaceBetween: 40
+            },
+            1024: {
+              slidesPerView: carousel.dataset.desktopColumns,
+              spaceBetween: 40
             },
           },
-          // when window width is >= 480px
-          480: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-            navigation: {
-              enabled: false,
-            },
-          },
-          // when window width is >= 640px
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 40
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 40
-          },
-        },
+      };
+
+      const carouselSlider = new Swiper(`.wsu-carousel-${index}`, swiperSettings);
+
     });
-
+  }
 }
 initSwiper(); 
