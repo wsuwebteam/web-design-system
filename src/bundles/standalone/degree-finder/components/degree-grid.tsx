7fe03eae14e289e09573c9c9fa60b66377a5317e
Dan White <dan.white1@wsu.edu>
2023-11-30 12:15:24 -0800

    feat: implement an intersection observer for conditional rendering to prevent lag

--- src/bundles/standalone/degree-finder/components/degree-grid.tsx
+++ src/bundles/standalone/degree-finder/components/degree-grid.tsx
@@ -1,3 +1,4 @@
+import { useInView } from "react-intersection-observer";
 import { degreeCollectionType, degreeType } from "../degrees";
 
 function DegreeGrid({ degrees, favorites, toggleFavorite }: { degrees: degreeCollectionType, favorites: number[], toggleFavorite: (id: number) => void }) {
@@ -6,25 +7,41 @@ function DegreeGrid({ degrees, favorites, toggleFavorite }: { degrees: degreeCol
         <div id="degreesGrid" className="wsu-card-group wsu-card-group--per-row-3">
             {degrees.map((d) => {
                 const isFavorited = favorites.includes(d.id);
-
-                return <article key={d.id} className="wsu-card wsu-degree-profile__related-card">
-                    <div className="wsu-image-frame wsu-image-frame--fixed-ratio">
-                        <a href={d.url} tabIndex={-1} aria-hidden="true" dangerouslySetInnerHTML={{ __html: d.image }}></a>
-                    </div>
-                    <div className="wsu-card__content">
-                        <h2 className="wsu-title"><a href={d.url}>{d.title}</a></h2>
-                        <span className="fa-layers fa-fw " role="listbox">
-                            <span className="wsu-screen-reader-only">{isFavorited ? 'Remove from' : 'Add to'} favorites</span>
-                            <span onClick={() => toggleFavorite(d.id)} className="wsu-degree-grid__favorite-controls" role="option" aria-selected={isFavorited} tabIndex={0}>
-                                <i className="fa-solid fa-heart fa-xl wsu-degree-grid__favorite-icon-solid"></i>
-                                <i className="fa-regular fa-heart fa-xl wsu-degree-grid__favorite-icon-regular"></i>
-                            </span>
-                        </span>
-                    </div>
-                </article>
+                return <Degree key={d.id} degree={d} isFavorited={isFavorited} toggleFavorite={toggleFavorite} />;
             })}
         </div>
     </div>
+
+}
+
+function Degree({ degree, isFavorited, toggleFavorite }: { degree: degreeType, isFavorited: boolean, toggleFavorite: (id: number) => void }) {
+
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        rootMargin: '200px 0px',
+    });
+
+    return <article ref={ref} className="wsu-card wsu-degree-profile__related-card">
+        {inView && <>
+            <div className="wsu-image-frame wsu-image-frame--fixed-ratio">
+                <a href={degree.url} tabIndex={-1} aria-hidden="true" dangerouslySetInnerHTML={{ __html: degree.image }}></a>
+            </div>
+        </>
+        }
+        <div className="wsu-card__content">
+            <h2 className="wsu-title"><a href={degree.url}>{degree.title}</a></h2>
+            {inView && <>
+                <span className="fa-layers fa-fw " role="listbox">
+                    <span className="wsu-screen-reader-only">{isFavorited ? 'Remove from' : 'Add to'} favorites</span>
+                    <span onClick={() => toggleFavorite(degree.id)} className="wsu-degree-grid__favorite-controls" role="option" aria-selected={isFavorited} tabIndex={0}>
+                        <i className="fa-solid fa-heart fa-xl wsu-degree-grid__favorite-icon-solid"></i>
+                        <i className="fa-regular fa-heart fa-xl wsu-degree-grid__favorite-icon-regular"></i>
+                    </span>
+                </span>
+            </>}
+        </div>
+    </article>
+
 }
 
 export default DegreeGrid;
--- src/bundles/standalone/degree-finder/components/degree-list.tsx
+++ src/bundles/standalone/degree-finder/components/degree-list.tsx
@@ -1,4 +1,5 @@
 import { degreeCollectionType, degreeType } from "../degrees";
+import { useInView } from "react-intersection-observer";
 
 function DegreeList({ degrees, favorites, toggleFavorite }: { degrees: degreeCollectionType, favorites: number[], toggleFavorite: (id: number) => void }) {
 
@@ -6,28 +7,44 @@ function DegreeList({ degrees, favorites, toggleFavorite }: { degrees: degreeCol
         <div id="degreeslist" className="wsu-card-group wsu-card-group--per-row-1">
             {degrees.map((d) => {
                 const isFavorited = favorites.includes(d.id);
-
-                return <article key={d.id} className="wsu-card wsu-degree-profile__related-card wsu-card--style-horizontal-25">
-                    <div className="wsu-image-frame wsu-image-frame--fixed-ratio">
-                        <a href={d.url} tabIndex={-1} aria-hidden="true" dangerouslySetInnerHTML={{ __html: d.image }}></a>
-                    </div>
-                    <div className="wsu-card__content">
-                        <div className="wsu-card__header">
-                            <h2 className="wsu-title"><a href={d.url}>{d.title}</a></h2>
-                            <span className="fa-layers fa-fw " role="listbox">
-                                <span className="wsu-screen-reader-only">{isFavorited ? 'Remove from' : 'Add to'} favorites</span>
-                                <span onClick={() => toggleFavorite(d.id)} className="wsu-degree-grid__favorite-controls" role="option" aria-selected={isFavorited} tabIndex={0}>
-                                    <i className="fa-solid fa-heart fa-xl wsu-degree-grid__favorite-icon-solid"></i>
-                                    <i className="fa-regular fa-heart fa-xl wsu-degree-grid__favorite-icon-regular"></i>
-                                </span>
-                            </span>
-                        </div>
-                        {d.excerpt && <p>{d.excerpt}</p>}
-                    </div>
-                </article>
+                return <Degree key={d.id} degree={d} isFavorited={isFavorited} toggleFavorite={toggleFavorite} />;
             })}
         </div>
     </div>
+
+}
+
+function Degree({ degree, isFavorited, toggleFavorite }: { degree: degreeType, isFavorited: boolean, toggleFavorite: (id: number) => void }) {
+
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        rootMargin: '400px 0px',
+    });
+
+    return <article ref={ref} className="wsu-card wsu-degree-profile__related-card wsu-card--style-horizontal-25">
+        {inView && <>
+            <div className="wsu-image-frame wsu-image-frame--fixed-ratio">
+                <a href={degree.url} tabIndex={-1} aria-hidden="true" dangerouslySetInnerHTML={{ __html: degree.image }}></a>
+            </div>
+        </>
+        }
+        <div className="wsu-card__content">
+            <div className="wsu-card__header">
+                <h2 className="wsu-title"><a href={degree.url}>{degree.title}</a></h2>
+                {inView && <>
+                    <span className="fa-layers fa-fw " role="listbox">
+                        <span className="wsu-screen-reader-only">{isFavorited ? 'Remove from' : 'Add to'} favorites</span>
+                        <span onClick={() => toggleFavorite(degree.id)} className="wsu-degree-grid__favorite-controls" role="option" aria-selected={isFavorited} tabIndex={0}>
+                            <i className="fa-solid fa-heart fa-xl wsu-degree-grid__favorite-icon-solid"></i>
+                            <i className="fa-regular fa-heart fa-xl wsu-degree-grid__favorite-icon-regular"></i>
+                        </span>
+                    </span>
+                </>}
+            </div>
+            {degree.excerpt && <p>{degree.excerpt}</p>}
+        </div>
+    </article>
+
 }
 
 export default DegreeList;
